6ba4dbdee246e06dc73e7180239d6058ef503b90
ravikoppula <ravikoppula92@gmail.com>
2023-08-18 17:15:39 +0800

    categerymodule

---
+++ DemoAPI/Models/Category.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DemoAPI.Models
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+    }
+
+    public interface ICategoryRepository
+    {
+        List<Category> GetCategories();
+    }
+
+
+    public class CategoryRepository : ICategoryRepository
+    {
+        public List<Category> GetCategories()
+        {
+            List<Category> categories = new List<Category>();
+
+            Category category = new Category() { CategoryId = 1, CategoryName = "Category1" };
+            categories.Add(category);
+
+            category = new Category() { CategoryId = 2, CategoryName = "Category2" };
+            categories.Add(category);
+
+            category = new Category() { CategoryId = 3, CategoryName = "Category3" };
+            categories.Add(category);
+
+            return categories;
+        }
+    }
+
+
+}
